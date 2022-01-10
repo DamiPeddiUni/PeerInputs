ac250315a362ba9418233a2cb196b240fead9dbe
carmelodfr <carmelodfr@gmail.com>
2022-01-10 22:21:13 +0100

    adding ping, connect,disconnet

--- PeerInputs/GameController/Client.gd
+++ PeerInputs/GameController/Client.gd
@@ -14,23 +14,24 @@ onready var BigButton_PANEL = get_parent().get_node("CanvasLayer/BigBtn");
 
 signal connected
 
-
+var isDemo = true
 var udp := PacketPeerUDP.new()
 var connected = false
-
-var noInput = "0" 
-var justPressed = "1"
-var pressed = "2"
-var justReleased = "3"
+onready var pingTime = OS.get_ticks_msec() 
 var prevCmd = "cmd:"
+
+
+enum enumState { state_notPressed,state_justPressed,state_pressed,state_released}
  
-var buttonA  = noInput
-var buttonB  = noInput
-var bigButton = noInput
+var buttonA   = enumState.state_notPressed
+var buttonB   = enumState.state_notPressed
+var bigButtonState = enumState.state_notPressed
 var startBigButtonPressedTime
+
+
 var quadrantVibrationTime : int = 40
 var oldQuadrant = -1
-var ipAddress
+var ipAddress = "192.168.1.68"
 
 func _ready():
 	Joystick_PANEL.set_visible(false);
@@ -38,9 +39,10 @@ func _ready():
 	BigButton_PANEL.set_visible(false);
 	Connect_PANEL.set_visible(true);
 
-	
 func _process(delta):
-	send_stuff(joystick.get_value());
+	sendData();
+	checkForMessages()
+	checkServerAlive()
 	
 func _on_ConnectBtn_pressed():
 	
@@ -50,85 +52,61 @@ func _on_ConnectBtn_pressed():
 		udp.put_packet("Connection request".to_utf8())
 		yield(get_tree().create_timer(0.5), "timeout")
 
-		if udp.get_available_packet_count() > 0:
+		if udp.get_available_packet_count() > 0 or isDemo:
 			myID = udp.get_packet().get_string_from_utf8()
 			ipAddress = textEdit.text
 			print("Connected to the server: %s" % myID)
 			connected = true
+			pingTime = OS.get_ticks_msec() 
 			emit_signal("connected")
 			Joystick_PANEL.set_visible(true);
 			Buttons_PANEL.set_visible(false);
 			BigButton_PANEL.set_visible(true);
-			Connect_PANEL.set_visible(false);	 
+			Connect_PANEL.set_visible(false);	
+			 
 			print(myID);
 		else:
 			print("server not found")
  
 func disconnectClient():
 	connected = false  
-	pass
+	get_tree().reload_current_scene();
+	print(ipAddress)
+	textEdit.set_text(ipAddress)
+	
 
-func send_stuff(data):
+func checkServerAlive():
+	if (connected):
+		var timeNow = OS.get_ticks_msec()
+		if (timeNow - pingTime> 10000):
+			print("SERVER DISCONNECTED!")
+			disconnectClient()
+		
+		
+func checkForMessages():
+	if udp.get_available_packet_count() > 0:
+		var msg = udp.get_packet().get_string_from_utf8()
+		print("received: %s" % msg)
+		if msg == "ping":
+			pingTime = OS.get_ticks_msec() 			
+		if msg == "vibrate":
+			Input.vibrate_handheld(200)
+
+func sendData():
 	
 	if !connected:
 		return;
 	
-	
-#	if data[0] > thres:
-#		if oldSentPos[0] < thres and oldSentPos[0] > -thres:
-#			stringCmd += justPressed	
-#		else:
-#			stringCmd += pressed
-#	elif data[0] < thres and data[0] > -thres  and oldSentPos[0] > thres:
-#		stringCmd += justReleased
-#	else:
-#		stringCmd += noInput
-#
-#	if data[0] < -thres:
-#		if oldSentPos[0] < thres and oldSentPos[0] > -thres:
-#			stringCmd += justPressed	
-#		else:
-#			stringCmd += pressed
-#	elif data[0] < thres and data[0] > -thres  and oldSentPos[0] < -thres:
-#		stringCmd += justReleased
-#	else:
-#		stringCmd += noInput
-#
-#	if data[1] > thres:
-#		if oldSentPos[1] < thres and oldSentPos[1] > -thres:
-#			stringCmd += justPressed	
-#		else:
-#			stringCmd += pressed
-#	elif data[1] < thres and data[1] > -thres  and oldSentPos[1] > thres:
-#		stringCmd += justReleased
-#	else:
-#		stringCmd += noInput
-#
-#	if data[1] < -thres:
-#		if oldSentPos[1] < thres and oldSentPos[1] > -thres:
-#			stringCmd += justPressed	
-#		else:
-#			stringCmd += pressed
-#	elif data[1] < thres and data[1] > -thres  and oldSentPos[1] < -thres:
-#		stringCmd += justReleased
-#	else:
-#		stringCmd += noInput
-#
+	var data = joystick.get_value()
+
 	handleVibration(data)
-	
-#
-	var stringCmd = "cmd:" + var2str(data) + ":"+ bigButton
-	
-	
-#	stringCmd += bigButton
-	
-	#print(data)
+	var stringCmd = "cmd:" + var2str(data) + ":"+ var2str(bigButtonState)
+
 	if ( prevCmd != stringCmd ):
 		udp.put_packet(stringCmd.to_utf8())
 		prevCmd = stringCmd
-		
-	#	print(stringCmd)
 	oldSentPos = data;
+	
 
 func handleVibration(data):
 	if (oldSentPos != data):
@@ -139,14 +117,10 @@ func handleVibration(data):
 		oldQuadrant = quadrant
 
 func computeQuadrant(data):
-	#var angles = [0, PI/4, PI/2 , 3/4*PI , PI,  5/4*PI, 6/4*PI, 7/4*PI, 2*PI ]
-	var angles =  [ -PI, -3/4*PI, - PI/2,  -PI/4,  0, PI/4, PI/2, 3/4*PI ]
-	
+	var angles =  [ -PI, -0.75*PI, - PI/2,  -PI/4,  0, PI/4, PI/2, 0.75*PI ]
+	 
 	var angle = atan2(data[1],data[0])
-#	print(angle)
-#	if (data[1] < 0):
-#		angle += PI
-	
+
 	var q = 0
 	for i in angles:
 		if angle > i:
@@ -155,32 +129,25 @@ func computeQuadrant(data):
 	
 	
 func _on_A_Button_pressed():
-	buttonA = pressed
+	buttonA = enumState.state_pressed
 
 func _on_B_Button_pressed():
-	buttonB = pressed
+	buttonB = enumState.state_pressed
 
 func _on_RefreshBtn_pressed():
+	udp.put_packet("disconnect:".to_utf8())
 	disconnectClient();
-	get_tree().reload_current_scene();
-	print(ipAddress)
-	#textEdit.text = ipAddress
-	#yield(get_tree().create_timer(0.5), "timeout")
 	
-	textEdit.append_at_cursor(ipAddress)
 	
-
 func _on_BigBtn_pressed():
-	#OS.vibrate_handheld(500)
 	startBigButtonPressedTime = OS.get_ticks_msec()
-	if (bigButton == justPressed):
-		bigButton = pressed
-		
+	if (bigButtonState == enumState.state_justPressed):
+		bigButtonState = enumState.state_pressed
 	else:
-		bigButton = justPressed
+		bigButtonState = enumState.state_justPressed
 
 func _on_BigBtn_released():
-	bigButton = justReleased
+	bigButtonState = enumState.state_released
 	var releaseTime = OS.get_ticks_msec()
 	var vibrateFor = min(releaseTime - startBigButtonPressedTime, 500)
 	print("vibrateFor" + str(vibrateFor))
